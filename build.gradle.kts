9ffaf11c40a84152e09944308c2252b24b97cf53
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-21 21:07:48 +0530

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 }
 dependencies {
     testImplementation(kotlin("test-junit"))
+    implementation(kotlin("script-runtime"))
 }
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"

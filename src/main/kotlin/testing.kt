e779425a770587aae2473043049c494acf5e8398
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-22 20:02:58 +0530

    updated

--- src/main/kotlin/testing.kt
+++ src/main/kotlin/testing.kt
@@ -1,11 +1,23 @@
 fun main(args : Array<String>) {
-    var _name = setName();
-    _name.name = "sanju";
-    // test class object
-//    println("my name is "+ _name.name);
-    println("my name is  ${_name.name}")
+//    var _name = setName();
+//    _name.name = "sanju";
+//    // test class object
+////    println("my name is "+ _name.name);
+//    println("my name is  ${_name.name}")
+//
+//    var ageGet = javaClass();
+//    ageGet.age = "34 years"
+//    println("my age is ${ageGet.age}");
+    var num1 : Int = 4;
+    var num2 : Int = 10;
+    var result = num1 + num2;
+    println("addtion value of ${num1} and ${num2} is ${result}")
 
-    var ageGet = javaClass();
-    ageGet.age = "34 years"
-    println("my age is ${ageGet.age}");
+    if(num1 > num2) {
+        println("$num1 is greater than $num2")
+    } else {
+        println("$num2 is greater than $num1")
+    }
+    
+
 }

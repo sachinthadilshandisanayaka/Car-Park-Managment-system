d9ab59748c3a551e9364d4571bfe652f8252aced
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-25 10:56:24 +0530

    file read package added

---
+++ src/main/kotlin/fileHandler/ReadFile.kt
@@ -0,0 +1,17 @@
+package fileHandler
+
+import java.io.File
+
+fun readFile() : MutableList<String>? {
+    var data = mutableListOf<String>()
+    val fileName : String = "C:\\Users\\sachi dissanayake\\IdeaProjects\\testKotlin\\src\\main\\kotlin\\fileHandler\\VehicleData.txt"
+    try {
+        val file = File(fileName)
+        file.forEachLine{
+            data.add(it)
+        }
+    } catch (e: Exception) {
+        println("The system cannot find the file")
+    }
+    return data
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,35 +1,37 @@
-
+import fileHandler.readFile
 fun main(args : Array<String>) {
-    println("Vehicle Park Management System")
-    println("1. Park \n2. Out\n3. Bill")
-    print("Enter your requirement :")
-    var oder = readLine()
-
-    if(oder.equals("park", true)) {
-
-    } else if(oder.equals("out", true)) {
-
-    } else if(oder.equals("bill", true)) {
-
-    } else {
-
-    }
-
-    println("1. Car\n2. Van\n3. ThreeWeel\n4. Bike\n5. LongVehicle")
-    print("Enter vehicle type :")
-    var vehicleType = readLine();
-    println(vehicleType)
-    if(vehicleType.equals("car", true)) {
-
-    } else if(vehicleType.equals("van", true)) {
-
-    } else if (vehicleType.equals("threeWeel", true)) {
-
-    } else if (vehicleType.equals("bike", true)) {
-
-    } else if (vehicleType.equals("longvehicle", true)) {
-
-    } else {
-
-    }
+    var readFileObject: MutableList<String>? = readFile()
+    println(readFileObject?.toString() + "\n" + readFileObject?.size)
+//    println("Vehicle Park Management System")
+//    println("1. Park \n2. Out\n3. Bill")
+//    print("Enter your requirement :")
+//    var oder = readLine()
+//
+//    if(oder.equals("park", true)) {
+//
+//    } else if(oder.equals("out", true)) {
+//
+//    } else if(oder.equals("bill", true)) {
+//
+//    } else {
+//
+//    }
+//
+//    println("1. Car\n2. Van\n3. ThreeWeel\n4. Bike\n5. LongVehicle")
+//    print("Enter vehicle type :")
+//    var vehicleType = readLine();
+//    println(vehicleType)
+//    if(vehicleType.equals("car", true)) {
+//
+//    } else if(vehicleType.equals("van", true)) {
+//
+//    } else if (vehicleType.equals("threeWeel", true)) {
+//
+//    } else if (vehicleType.equals("bike", true)) {
+//
+//    } else if (vehicleType.equals("longvehicle", true)) {
+//
+//    } else {
+//
+//    }
 }

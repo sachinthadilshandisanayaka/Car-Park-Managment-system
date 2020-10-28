0f8f884920bd49177cac44d2c606c2c623b46ce8
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-29 00:00:01 +0530

    updated

--- src/main/kotlin/operation/GetOutVehicle.kt
+++ src/main/kotlin/operation/GetOutVehicle.kt
@@ -2,7 +2,10 @@ package operation
 
 import fileHandler.readFile
 import operation.timeZone.CurrentState
+import sun.util.resources.LocaleData
 import ui.VehicleEmpty
+import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class GetOutVehicle() :AddBillOperation{
@@ -16,8 +19,8 @@ class GetOutVehicle() :AddBillOperation{
                     if(splitData[0].equals(vType, true) && splitData[3].equals(vNumber, true)) {
                         var currentTime = CurrentState().currentTimeAndDate()
                         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
-                        var parkingTimeZone = splitData[5].format(formatter)
-                        println(parkingTimeZone + " and " + currentTime?.format(formatter))
+                        var parkingTimeZone = LocalDateTime.parse(splitData[5], formatter)
+                        println(" $currentTime and $parkingTimeZone")
                         checker = 1;
                     }
             }

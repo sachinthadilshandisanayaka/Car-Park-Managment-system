7c31bb58103e1078e77656a650b9d5dd943a3563
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-11-04 21:26:23 +0530

    updated

---
+++ src/main/kotlin/dataServer/mysqlDataBase/connection/quary/InsertQuery.kt
@@ -0,0 +1,8 @@
+package dataServer.mysqlDataBase.connection.quary
+
+class InsertQuery {
+    private val query = "INSERT INTO CarPark('') VALUE('')"
+    fun getQuery() :String {
+        return query
+    }
+}
